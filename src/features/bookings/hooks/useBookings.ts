eada4ad922133054f396f0ce69f512cc274beff2
Ryan Su <isRyanSu@gmail.com>
2024-05-26 15:31:36 +0800

    feat(bookings): pre-fetching the bookings

--- src/features/bookings/hooks/useBookings.ts
+++ src/features/bookings/hooks/useBookings.ts
@@ -1,10 +1,13 @@
 import { useSearchParams } from 'react-router-dom'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 import getAllBookings from '@/features/bookings/api/getAllBookings'
 
+import { PAGE_SIZE } from '@/utils/constants'
+
 function useBookings() {
   const [searchParams] = useSearchParams()
+  const queryClient = useQueryClient()
 
   // FILTER
   const filterValue = searchParams.get('status')
@@ -21,7 +24,7 @@ function useBookings() {
   // PAGINATION
   const page: number = !searchParams.get('page')
     ? 1
-    : Number(searchParams.get('page'))
+    : Number(searchParams.get('page')) //记录当前页数
 
   const {
     isPending: isLoadingAllBookings,
@@ -31,6 +34,24 @@ function useBookings() {
     queryKey: ['Bookings', filter, sorter, page], // 以非常优雅的方式来触发重新验证（如果 filter 或 sorter 发生变化）
     queryFn: () => getAllBookings({ filter, sorter, page }),
   })
+
+  // PRE-FETCHING
+  const pageCount: number = Math.ceil((numBookings || 0) / PAGE_SIZE) // 记录总页数
+
+  // 如果当前页不是最后一页，则预加载下一页
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ['Bookings', filter, sorter, page + 1],
+      queryFn: () => getAllBookings({ filter, sorter, page: page + 1 }),
+    })
+  }
+
+  // 如果当前页不是第一页，则预加载上一页
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ['Bookings', filter, sorter, page - 1],
+      queryFn: () => getAllBookings({ filter, sorter, page: page - 1 }),
+    })
 
   return { isLoadingAllBookings, allBookings, numBookings, getAllBookingsError }
 }
