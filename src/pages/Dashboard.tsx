ac04880acac3eaf2c4571676fcefd5a38433503f
Ryan Su <isRyanSu@gmail.com>
2024-05-27 23:10:16 +0800

    chore(dashboard): build the dashboard layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,19 @@
+import DashboardFilter from '@/features/dashboard/components/DashboardFilter'
+import DashboardLayout from '@/features/dashboard/components/DashboardLayout'
+
 import Heading from '@/components/Heading'
 import Row from '@/components/Row'
 
 function Dashboard() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+
+      <DashboardLayout />
+    </>
   )
 }
 
