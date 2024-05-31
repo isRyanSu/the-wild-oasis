6e76047da92794df02fded37832188706e3fcbdb
Ryan Su <isRyanSu@gmail.com>
2024-06-01 04:22:31 +0800

    chore: forget to submit this component
    - DashboardBox

---
+++ src/features/dashboard/components/DashboardBox.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const DashboardBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+  padding: 3.2rem;
+
+  /* Box */
+  background-color: var(--color-grey-0);
+  border: 1px solid var(--color-grey-100);
+  border-radius: var(--border-radius-md);
+`
+
+export default DashboardBox
